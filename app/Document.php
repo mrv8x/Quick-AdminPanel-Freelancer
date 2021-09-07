ac2735236513cee9309e574249a930a627c72150
mrv8x <57045314+mrv8xx@users.noreply.github.com>
2021-09-07 21:05:33 +0100

    First commit

---
+++ app/Document.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Document extends Model implements HasMedia
+{
+    use SoftDeletes, InteractsWithMedia;
+
+    public $table = 'documents';
+
+    protected $appends = [
+        'document_file',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'project_id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'description',
+    ];
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')->width(50)->height(50);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'project_id');
+    }
+
+    public function getDocumentFileAttribute()
+    {
+        return $this->getMedia('document_file')->last();
+    }
+}
